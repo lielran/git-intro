81595855fe7d200dc2c3221dcaf4449c0d4d3fdf
lielran <lielran86>
2013-11-07 09:19:48 +0200

    remove var

--- angular.js
+++ angular.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var x =5;
 var webapp =angular.module('webApp', []).
     config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/home', {templateUrl: 'index.html', controller: 'EventController'});
