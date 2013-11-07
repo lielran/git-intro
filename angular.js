7cdcc1c11dcd02569ef445318dd06f55e1e0fa2a
liel ran <lielran@liels-MacBook-Pro.local>
2013-11-07 09:13:25 +0200

    First commit

---
+++ angular.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+
+
+var webapp =angular.module('webApp', []).
+    config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/home', {templateUrl: 'index.html', controller: 'EventController'});
+        $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+        $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+        $routeProvider.otherwise({redirectTo: '/view1'});
+    }]);
